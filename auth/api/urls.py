b76b4eef36db4c6243ff7f8e209abc4dd7765154
zyadmayad <zyadmayad@outlook.com>
2026-04-09 16:55:16 +0200

    inhancement structure

--- auth/api/urls.py
+++ auth/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from auth.api.views import user_detail, user_list
+from auth.api.views import current_user_detail, user_detail, user_list
 
 urlpatterns = [
 	path('users/', user_list, name='users_api_list'),
+	path('user/account/', current_user_detail, name='users_api_account'),
 	path('users/<int:user_id>/', user_detail, name='users_api_detail'),
 ]
