d4b2ad438c9f3df93b840cc0493f74dbaa3ef9cf
zyadmayad <zyadmayad@outlook.com>
2026-04-12 17:47:53 +0200

    fix structures and layout urls

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from auth.models import Users
-from auth.permissions import IsAdmin
+from auth.permissions import IsAdmin, IsDoctor
 
 # Create your views here.
 def home(request):
@@ -19,6 +19,25 @@ def admin_dashboard(request):
   return render(
       request,
       "admin/admin_dashboard.html",
+      {
+          "user_name": user_name,
+          "user_role": user_role,
+      },
+  )
+  
+def doctor_dashboard(request):
+  if not IsDoctor().has_permission(request, None):
+    if not request.user.is_authenticated:
+      return redirect('auth:login')
+    return redirect('home')
+
+  profile = Users.objects.filter(user=request.user).only('role').first()
+  user_name = request.user.username
+  user_role = profile.role
+
+  return render(
+      request,
+      "doctor/doctor-dashboard.html",
       {
           "user_name": user_name,
           "user_role": user_role,
