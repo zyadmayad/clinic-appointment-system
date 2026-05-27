2685c676e16b79c20b7fbd67c083c3e8800b896a
zyadmayad <zyadmayad@outlook.com>
2026-05-28 00:56:43 +0200

    update auth logic to use django auth system and permissions and create the admin board with static data

--- admin/urls.py
+++ admin/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import admin_dashboard
+from .import views
 
 app_name = "clinic_admin"
 
 urlpatterns = [
-    path("", admin_dashboard, name="admin_dashboard"),
+    path("", views.admin_dashboard, name="admin_dashboard"),
 ]
--- admin/views.py
+++ admin/views.py
@@ -1,24 +1,13 @@
 from django.shortcuts import redirect, render
-from django.urls import reverse
-from django.views.decorators.cache import never_cache
+from auth.permissions import IsAdmin
 
 # Create your views here.
 def home(request):
   return render(request, "home.html")
 
-@never_cache
 def admin_dashboard(request):
-  user_id = request.session.get("user_id")
-  user_role = request.session.get("user_role")
-
-  if not user_id or user_role != "admin":
-    return redirect("auth:login")
-
-  context = {
-    "user_id": user_id,
-    "user_name": request.session.get("user_name", "Admin"),
-    "user_role": user_role.title(),
-    "user_email": request.session.get("user_email", "Admin"),
-    "user_detail_api_url": reverse("users_api_detail", args=[user_id]),
-  }
-  return render(request, "admin_dashboard.html", context)
+  if not IsAdmin().has_permission(request, None):
+    if not request.user.is_authenticated:
+      return redirect('auth:login')
+    return redirect('home')
+  return render(request, "admin_dashboard.html")
