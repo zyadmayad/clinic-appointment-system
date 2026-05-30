7f2397c8875d95ea8c602e6c994b8304b120cfc9
OmarAliSiad <omaraliabdelaziz250@gmail.com>
2026-05-30 17:13:24 +0200

    fix: admin dashboard

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from auth.models import Users
 from auth.permissions import IsAdmin
 
 # Create your views here.
@@ -10,4 +11,16 @@ def admin_dashboard(request):
     if not request.user.is_authenticated:
       return redirect('auth:login')
     return redirect('home')
-  return render(request, "admin_dashboard.html")
+
+  profile = Users.objects.filter(user=request.user).only('role').first()
+  user_name = request.user.username
+  user_role = profile.role
+
+  return render(
+      request,
+      "admin/admin_dashboard.html",
+      {
+          "user_name": user_name,
+          "user_role": user_role,
+      },
+  )
