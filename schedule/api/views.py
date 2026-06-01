3f9973ef9d5aab4568a4f673f928145334324089
OmarAliSiad <omaraliabdelaziz250@gmail.com>
2026-06-01 20:38:46 +0200

    feat: Add patient queue and consultation views, update dashboard links, and enhance schedule management

--- schedule/api/views.py
+++ schedule/api/views.py
@@ -10,11 +10,13 @@ from rest_framework.decorators import api_view
 def schedule_list(request,doctor_id):
 
     if request.method == 'POST':
-        schedule = ScheduleSerializer(data=request.data)
-        if schedule.is_valid():
-            schedule.save(doctor_id=doctor_id)
-            return Response(schedule.data, status=status.HTTP_201_CREATED)
-
+        try:
+            schedule = ScheduleSerializer(data=request.data)
+            if schedule.is_valid():
+                schedule.save(doctor_id=doctor_id)
+                return Response(schedule.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
     schedules = Schedule.objects.filter(doctor_id=doctor_id)
     serializer = ScheduleSerializer(schedules, many=True)
@@ -28,8 +30,7 @@ def schedule_exception(request, doctor_id):
     print(schedule)
     try:
         schedule_obj = Schedule.objects.get(id=schedule, doctor_id=doctor_id)
-        schedule_obj.day_type = request.data.get('day_type')
-        schedule_obj.save()
+        schedule_obj.delete()
         serializer = ScheduleSerializer(schedule_obj)   
         return Response(serializer.data, status=status.HTTP_200_OK)
     
