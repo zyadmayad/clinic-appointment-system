ebdf79a77c93562f670aaf33edb80b5bf67806ad
elsayedfarg <elsayedfarg792003@gmail.com>
2026-06-03 22:31:30 +0200

    Refactor queue_management to API package structure

--- queue_management/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from queue_management.views import CheckInView, QueueManageView, TodayQueueView
-
-urlpatterns = [
-    path('check-in/', CheckInView.as_view(), name='queue_check_in'),
-    path('today/', TodayQueueView.as_view(), name='queue_today'),
-    path('<int:appointment_id>/', QueueManageView.as_view(), name='queue_manage'),
-]
