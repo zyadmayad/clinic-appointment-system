0f80a14d79185c858c49ebf0935d081a1e68bd5e
zyadmayad <zyadmayad@outlook.com>
2026-06-05 00:00:00 +0200

    remove caches

--- appointment/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from appointment.models import Appointment, AppointmentHistory
-
-admin.site.register(Appointment)
-admin.site.register(AppointmentHistory)
-
--- auth/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from auth.models import Users
-
-# Register your models here.
-@admin.register(Users)
-class UsersAdmin(admin.ModelAdmin):
-	list_display = ('id', 'user', 'role')
-	list_filter = ('role',)
-	search_fields = ('user__username', 'user__email')
