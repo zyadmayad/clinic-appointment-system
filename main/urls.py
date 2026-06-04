f4cefb0e29d4fbe3887b2b60c0f9fd9352871e57
zyadmayad <zyadmayad@outlook.com>
2026-06-05 00:00:00 +0200

    remove caches

---
+++ main/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import include, path
+from auth.views import home
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('', include('auth.urls')),
+    path('dashboard/', include(('dashboards.urls', 'dashboards'), namespace='dashboards')),
+    path('dashboard/doctor/', include(('schedule.urls', 'schedule'), namespace='schedule')),
+    path('dashboard/patient/', include(('appointment.urls', 'appointment'), namespace='appointment')),
+    path('api/doctors/', include('schedule.api.urls')),
+    path('api/appointments/', include('appointment.api.urls')),
+    path('api/queue/', include('queue_management.urls')),
+    path('api/', include('auth.api.urls')),
+]
