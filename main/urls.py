7b954f6b4f5d43a01c1d17f00c1ee681ab8c1edf
zyadmayad <zyadmayad@outlook.com>
2026-06-10 11:30:06 +0200

    enhance landing page and allow the permission in admin views

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from auth.views import home
 
 urlpatterns = [
@@ -21,3 +23,6 @@ urlpatterns = [
     path('api/', include('auth.api.urls')),
     path("accounts/", include("allauth.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
